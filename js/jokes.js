547af01bb37d79e50392c2ac76cd8d1ff4dcdcd3
Leah Sanchez <111553190+snoberry@users.noreply.github.com>
2023-03-20 21:47:36 +0000

    Mon, Mar 20, 2023, 2:47 PM -07:00

--- js/jokes.js
+++ js/jokes.js
@@ -1,11 +1,14 @@
 const jokeEl = document.getElementById('joke')
 const jokeBtn = document.getElementById('jokeBtn')
 
+generateJoke()
+
 jokeBtn.addEventListner('click', generateJoke)
 
 async function generateJoke(){
     const response = await fetch('https://icanhazddjoke.com',{
         headers: {Accept: 'application/json'}
     })
-    
+    const data = await response.json()
+    jokeEl.innerHTML = data.joke
 }
