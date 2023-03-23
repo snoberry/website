39480f197167201a98f65882a4c5be05d2a8af77
Leah Sanchez <111553190+snoberry@users.noreply.github.com>
2023-03-23 21:38:51 +0000

    Thu, Mar 23, 2023, 2:38 PM -07:00

--- js/progress.js
+++ js/progress.js
@@ -1,47 +1,53 @@
 const prev = document.getElementById('prev')
 const next = document.getElementById('next')
 const progress = document.getElementById('progress')
-const circles =document.querySelectorAll('.circle')
+const circles = document.querySelectorAll('.circle')
+
+
 
 
 let currentActive = 1
 
-next.addEventListener('click', () => {
-    currentActive++
 
-    if (curentActive > 4) {
+next.addEventListener('click', () => {
+    currentActive ++
+    if (currentActive > 4) {
         currentActive = 4
     }
     update()
 })
 
-prev.next.addEventListener('click', () => {
-    currentActive--
-    if (curentActive < 1) {
+
+prev.addEventListener('click', () => {
+    currentActive --
+    if (currentActive < 1) {
         currentActive = 1
     }
     update()
 })
 
-function update(){
-    progress.style.width= (currentActive-1)/3*100 + '%'
 
-    if (currentActive == 1){
-        prev.disbled = true
+function update() {
+    progress.style.width = (currentActive - 1)/3*100 + '%';
+
+
+    if (currentActive == 1) {
+        prev.disabled = true
     }
-    else if (currentActive == 4){
+    else if (currentActive == 4) {
         next.disabled = true
     }
     else {
-        prev.disabled = false
-        next.disabled = false
+        prev.disabled = false;
+        next.disabled = false;
     }
 
+
     circles.forEach((circle, index) => {
-        if (index < currentActive){
+        if (index < currentActive) {
             circle.classList.add('active')
         }
-        else{
+        else {
             circle.classList.remove('active')
         }
     })
